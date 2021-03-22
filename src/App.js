632787ecb178343a5d5f4d3682ec0fb81bc81415
Tolu Adeoye <toluade1512@gmail.com>
2021-03-22 14:37:29 +0100

    Setting up how the app will fetch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <p>Netflix Clone</p>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
