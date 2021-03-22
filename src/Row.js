b77277d3964c664ac429e4fdc4bc3ee9889e13d3
Tolu Adeoye <toluade1512@gmail.com>
2021-03-22 18:49:19 +0100

    Fix fetch data error

--- src/Row.js
+++ src/Row.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import "./Row.css";
 
-const base_url = "https://api.themoviedb.org/3";
+const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
