a04727332f7d82c8e938e872de88a52acdeede76
Tolu Adeoye <toluade1512@gmail.com>
2021-03-22 20:46:23 +0100

    Build banner component and fix row image issues

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
 
 function App() {
   return (
-    <div className="App">
-      <p>Netflix Clone</p>
+    <div className="app">
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
